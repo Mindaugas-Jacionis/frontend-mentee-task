62862e44596c7b56430d23d2d0436f8b9c829ad5
DanielDauk <danieldauk@gmail.com>
2018-06-09 11:47:10 +0300

    reducer tests

---
+++ src/store/reducer.test.js
@@ -0,0 +1,66 @@
+import reducer from "./reducer";
+import * as actionTypes from "./actionTypes";
+
+describe("reducer", () => {
+  it("should return initial state", () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: "",
+      servers: ""
+    });
+  });
+
+  it("should return initial state on logout", () => {
+    expect(
+      reducer(
+        {
+          token: "token",
+          servers: "servers"
+        },
+        {
+          type: actionTypes.AUTH_LOGOUT
+        }
+      )
+    ).toEqual({
+      token: "",
+      servers: ""
+    });
+  });
+
+  it("should store token on login", () => {
+    expect(
+      reducer(undefined, {
+        type: actionTypes.AUTH_SUCCESS,
+        token: "token"
+      })
+    ).toEqual({
+      token: "token",
+      servers: ""
+    });
+  });
+
+  it("should store servers and sort by distance and name", () => {
+    expect(
+      reducer(
+        {
+          token: "",
+          servers: ""
+        },
+        {
+          type: actionTypes.SAVE_SERVERS,
+          servers: [
+            { name: "a", distance: 100 },
+            { name: "c", distance: 200 },
+            { name: "b", distance: 200 }
+          ]
+        }
+      )
+    ).toEqual({
+      token: "",
+      servers: [
+        { name: "a", distance: 100 },
+        { name: "b", distance: 200 },
+        { name: "c", distance: 200 }
+      ]
+    });
+  });
+});
